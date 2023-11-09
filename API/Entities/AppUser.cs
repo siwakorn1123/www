1252df0387fae956926eacd7a7ce05a9d4d89e09
Siwakorn.Phum <150316488+siwakorn1123@users.noreply.github.com>
2023-11-09 21:28:03 +0700

    Initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    //snippet: typing "prop" then press tap
+    public int Id { get; set; }
+    public string? UserName { get; set; }
+}
